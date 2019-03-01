5889144e3e8f191a22feaa621d242123fb2db44f
dev <Illyakul1996@gmail.com>
2019-03-01 14:44:20 +0200

    migration to webhookbot

--- src/main/kotlin/org/lif/bots/LostInFutureBot.kt
+++ src/main/kotlin/org/lif/bots/LostInFutureBot.kt
@@ -2,6 +2,7 @@ package org.lif.bots
 
 import org.lif.commands.Commands
 import org.lif.savedTime
+import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.bots.TelegramWebhookBot
 
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot
@@ -13,21 +14,21 @@ import org.telegram.telegrambots.meta.api.objects.User
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 import org.telegram.telegrambots.meta.logging.BotLogger
 
-class LostInFutureBot : TelegramWebhookBot() {
-    override fun getBotPath() = "https://lifbot.herokuapp.com/"
+class LostInFutureBot : TelegramLongPollingBot() {
+//    override fun getBotPath() = "https://lifbot.herokuapp.com/"
     override fun getBotUsername() = "Michael"
 
 
     private val TAG = "LiFBot"
 
-
-    override fun onWebhookUpdateReceived(update: Update?): BotApiMethod<*> {
+    override fun onUpdateReceived(update: Update?) {
         if (update?.hasMessage()!! && update.message.hasText()) {
 
             var messageText: String? = null
             val chatId: String? = update.message.chatId.toString()
 
             if (isCommandForMe(update.message.text)) {
+//                System.out.println("LOGS = chatId $chatId")
                 when (update.message.text) {
                     Commands.reptime, Commands.reptimeGroup -> messageText =
                         executeRepTimeCommand(update.message.from, update.message.chat)
@@ -45,14 +46,52 @@ class LostInFutureBot : TelegramWebhookBot() {
 //                sendMessage("ПИДОР " + Emoji.HEAVY_BLACK_HEART, chatId)
 //            }
         }
-        return sendMessage("", "")
     }
 
-    private fun sendMessage(messageText: String?, chatId: String?): SendMessage {
+//    override fun onWebhookUpdateReceived(update: Update?): BotApiMethod<*> {
+//        if (update?.hasMessage()!! && update.message.hasText()) {
+//
+//            var messageText: String? = null
+//            val chatId: String? = update.message.chatId.toString()
+//
+//            if (isCommandForMe(update.message.text)) {
+////                System.out.println("LOGS = chatId $chatId")
+//                when (update.message.text) {
+//                    Commands.reptime, Commands.reptimeGroup -> messageText =
+//                        executeRepTimeCommand(update.message.from, update.message.chat)
+//                    Commands.setreptime, Commands.setreptimeGroup -> messageText =
+//                        executeSetTimeCommand(update.message.from, update.message.chat)
+//                }
+//                sendMessage(messageText, chatId)
+//            } else if (update.message.text.split("\\s".toRegex(), 2)[0].toLowerCase() == Commands.time) {
+//                savedTime = update.message.text.toLowerCase().substringAfter(Commands.time)
+//                messageText = "новое время репетиции $savedTime"
+//
+//                sendMessage(messageText, chatId)
+//            } //else if( update.message.text.toLowerCase().contains("артем")
+////                ||  update.message.text.toLowerCase().contains("артём")){
+////                sendMessage("ПИДОР " + Emoji.HEAVY_BLACK_HEART, chatId)
+////            }
+//        }
+//        return sendMessage("", "")
+//    }
+
+    private fun sendMessageWebHook(messageText: String?, chatId: String?): SendMessage {
         val message = SendMessage()
         message.chatId = chatId.toString()
         message.text = messageText
         return message
+
+    }
+    private fun sendMessage(messageText: String?, chatId: String?){
+        val message = SendMessage()
+        message.chatId = chatId.toString()
+        message.text = messageText
+        try{
+            execute(message)
+        }catch (e:TelegramApiException){
+            System.out.println(e.message)
+        }
 
     }
 
